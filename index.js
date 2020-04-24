641a77da3948c95876547dfc42bd4f2653ace947
Anurag <aknrg627@gmail.com>
2020-04-24 18:16:13 +0530

    13th

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const passport = require('passport');
 const googleStrategy = require('./config/passport-google-oauth2-strategy')
 const cookieSession = require('cookie-session');
