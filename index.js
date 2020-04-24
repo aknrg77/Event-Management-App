8db1c734f207675b8b5f6849956e6cf182629cb4
Anurag <aknrg627@gmail.com>
2020-04-24 06:17:27 +0530

    Changes made in herkou

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-Parser');
 const passport = require('passport');
 const googleStrategy = require('./config/passport-google-oauth2-strategy')
